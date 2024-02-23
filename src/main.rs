ea2b081abe4ed570193072bd00c29d24778c0710
NilsJR <42474434+sackidude@users.noreply.github.com>
2024-02-23 09:56:53 +0000

    date formatting

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use serde::Serialize;
 use serde_json::value::{Map, Value as Json};
+use chrono::Datelike;
 
 use std::fs::File;
 
@@ -8,30 +9,39 @@ use handlebars::{
 };
 
 #[derive(Serialize)]
-pub struct Game {
+struct Game {
+    num: u8,
     ally_champ: String,
     enemy_champ: String,
     scoreline: String,
 }
 
 #[derive(Serialize)]
-pub struct Session {
+struct Session {
     date: String,
     games: Vec<Game>,
 }
 
-pub fn fetch_data() -> Map<String, Json> {
+// This function should probably be slightly different.
+fn get_date() -> String {
+    let now = chrono::offset::Local::now();
+    return format!("{}/{}-{}", now.day().to_string(), now.month().to_string(), now.year().to_string())
+}
+
+fn fetch_data() -> Map<String, Json> {
     let mut data = Map::new();
 
-    data.insert("date".to_string(), to_json("2024-02-23"));
+    data.insert("date".to_string(), to_json(get_date()));
 
     let games = vec![
         Game {
+            num: 1,
             ally_champ: "Graves".to_string(),
             enemy_champ: "Master Yi".to_string(),
             scoreline: "2/7/6".to_string(),
         },
         Game {
+            num: 2,
             ally_champ: "Graves".to_string(),
             enemy_champ: "Rammus".to_string(),
             scoreline: "10/3/5".to_string(),
@@ -51,6 +61,7 @@ fn main() {
         .register_template_file("template", "./templates/template.hbs")
         .unwrap();
 
+    // TODO: Write the file in a appropriate folder/location.
     let output_file = match File::create("target/output.md") {
         Err(why)=>panic!("{}", why),
         Ok(f)=>f
